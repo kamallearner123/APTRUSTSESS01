817421200931986e32c21b6abb9aa568281ea1ef
kamallearner123 <kamalbec2004@gmail.com>
2024-10-02 18:41:16 +0530

    Adding for modules

---
+++ Classes/Kamal/Sep30_Modules_IntegerOverflow/LibAppTogether/idsm/src/main.rs
@@ -0,0 +1,6 @@
+use idsm::auth::add; // Import the function from the library
+
+fn main() {
+    add(1,2); // Call the library function
+    println!("Done!!!");
+}
---
+++ Classes/Kamal/Sep30_Modules_IntegerOverflow/LibAppTogether/idsm/target/debug/deps/idsm-575b6a76fa844fa2.d
@@ -0,0 +1,5 @@
+/Users/kamalmukiri/Documents/GitHub/Courses/APTRUSTSESS01/Classes/Kamal/Sep30_Modules_IntegerOverflow/LibAppTogether/idsm/target/debug/deps/idsm-575b6a76fa844fa2: src/main.rs
+
+/Users/kamalmukiri/Documents/GitHub/Courses/APTRUSTSESS01/Classes/Kamal/Sep30_Modules_IntegerOverflow/LibAppTogether/idsm/target/debug/deps/idsm-575b6a76fa844fa2.d: src/main.rs
+
+src/main.rs:
