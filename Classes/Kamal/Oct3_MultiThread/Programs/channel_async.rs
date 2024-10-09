2774c65f0df222b7842bb91e0625a037771ae255
kamallearner123 <kamalbec2004@gmail.com>
2024-10-09 11:53:05 +0530

    Multi-thread issues

--- Classes/Kamal/Oct3_MultiThread/Programs/channel_async.rs
+++ Classes/Kamal/Oct3_MultiThread/Programs/channel_async.rs
@@ -6,6 +6,13 @@ fn main() {
     // Create a channel
     let (tx, rx) = mpsc::channel();
 
+    // int rxtx1[2] = pipe();
+    // int rxtx2[2] = pipe();
+    // //  child: rxtx1[0] - read
+    // //  child: rxtx2[1] -  write
+    // //  parent: rxtx1[0] - write
+    // //  parent: rxtx2[1] - read
+
     thread::spawn(move || {
         let val = String::from("Hello from the thread!");
         // Send a value through the channel
